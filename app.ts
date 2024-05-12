51c7418345d4e2d0fafa2eae3c38cb7c4cd36a4e
student-Kiranrao <kr3119842@gmail.com>
2024-05-12 16:54:13 +0500

    push Array method

---
+++ app.ts
@@ -0,0 +1,33 @@
+// Array
+ 
+const foodItem = ["apple","Tomato","potato","chips"];
+console.log(foodItem);
+
+// Array Method
+// push(): add to end
+foodItem.push("mango","burger");
+console.log(foodItem);
+
+// pop(): delete from end & return
+foodItem.pop();
+console.log(foodItem);
+
+// concat(): joins multiple arrays & return result
+let marvelHeroes = ["spiderman","thor",'ironman'];
+let dc_heroes = ['superman','batman'];
+let heroes=marvelHeroes.concat(dc_heroes);
+console.log(heroes);
+
+// unshift(): add to start
+marvelHeroes.unshift("antman");
+console.log(marvelHeroes);
+
+// shift(): delete from start & return
+marvelHeroes.shift();
+console.log(marvelHeroes)
+
+
+
+
+
+
